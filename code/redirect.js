04b6fce1ae7b51be2ad9066784a0d63f4f4efef1
watermeloncool <149166592+watermeloncool@users.noreply.github.com>
2023-10-27 14:41:41 +0200

    Add files via upload

--- code/redirect.js
+++ code/redirect.js
@@ -1,5 +1,5 @@
 const lang = navigator.language.toLowerCase();
 if (lang.startsWith('fr')) {}
-else if (lang.startsWith('ru')) {window.location.href = 'https://watermelon.cool/ru/';}
-else if (lang.startsWith('de')) {window.location.href = 'https://watermelon.cool/de/';}
-else {window.location.href = 'https://watermelon.cool/en/';}
+else if (lang.startsWith('ru')) {window.location.href = '/ru';}
+else if (lang.startsWith('de')) {window.location.href = '/de';}
+else {window.location.href = '/en';}
